7d5653f699cd8c75feac6500b0686edc4f71e79a
Martin Weidhofer <martin.weidhofer@world-direct.at>
2026-04-28 11:40:55 +0200

    Update scripts with passable path and executable guards

--- csm/CsmServiceJMeterTests.ps1
+++ csm/CsmServiceJMeterTests.ps1
@@ -14,12 +14,19 @@
 # limitations under the License.
 #
 
+param(
+    [string]$JMeterExe = "$PSScriptRoot\apache-jmeter-5.6.3\bin\jmeter.bat"
+)
 
-$jmeterExe = "$PSScriptRoot\apache-jmeter-5.6.3\bin\jmeter.bat"
+# Validate JMeter executable
+if (-not (Test-Path -Path $JMeterExe -PathType Leaf)) {
+    Write-Error "JMeter executable not found at '$JMeterExe'"
+    exit 1
+}
 
 #$PSScriptRoot is the path of the current script
 $arguments = @()
 $arguments += "--addprop $PSScriptRoot\CsmServiceJMeterTests.properties"
 $arguments += "--testfile $PSScriptRoot\CsmServiceJMeterTests.jmx"
 
-Start-Process -FilePath $jmeterExe -ArgumentList $arguments -NoNewWindow
+Start-Process -FilePath $JMeterExe -ArgumentList $arguments -NoNewWindow
